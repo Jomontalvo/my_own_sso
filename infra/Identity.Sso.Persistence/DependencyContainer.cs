135aba2df40d856c9aa990c1266dc8074a82a05a
José Oswaldo Montalvo Espinoza <oswaldo.montalvo@sigob.org>
2026-09-21 15:45:04 -0500

    feat: Update DependencyContainer to register ApplicationDbContext and OpenIddict integration

--- infra/Identity.Sso.Persistence/DependencyContainer.cs
+++ infra/Identity.Sso.Persistence/DependencyContainer.cs
@@ -1,4 +1,7 @@
-
+using Identity.Sso.Persistence.Context;
+using Identity.Sso.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Identity.Sso.Persistence;
@@ -10,8 +13,28 @@ public static class DependencyContainer
     /// </summary>
     /// <param name="services">The service collection to which the persistence services will be added.</param>
     /// <returns>The updated service collection with the persistence services added.</returns>
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
+        // 1. Register ApplicationDbContex
+        var connectionString = configuration.GetConnectionString("IdentityConnection")
+            ?? throw new InvalidOperationException("Connection string 'IdentityConnection' not found.");
+
+        // 2. Factory (Singleton) for the seeder + DbContext (Scoped) for repositories and unit of work
+        // AddDbContextFactory registers both automatically from EF Core 10 
+        services.AddDbContextFactory<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.UseOpenIddict();
+        });
+
+        // 3. OpenIddict Core with EF Integration
+        services.AddOpenIddict()
+            .AddCore(options =>
+            {
+                options.UseEntityFrameworkCore()
+                    .UseDbContext<ApplicationDbContext>();
+            });
+
         return services;
     }
 
--- pres/api/Identity.Sso.Api/Program.cs
+++ pres/api/Identity.Sso.Api/Program.cs
@@ -1,12 +1,23 @@
+using Identity.Sso.Application;
+using Identity.Sso.Persistence;
+using Identity.Sso.Persistence.Context;
+using Identity.Sso.Persistence.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var config = builder.Configuration;
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(config);
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +25,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
